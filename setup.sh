0da3b81eda8b42d7060661fc00af2d79b5e329dc
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-06-01 10:11:32 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "hi"
+git clone --bare git@github.com:Hacksore/dotfiles.git $HOME/.cfg
+
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+mkdir -p .config-backup
+config checkout
+
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+
+config checkout
+config config status.showUntrackedFiles no

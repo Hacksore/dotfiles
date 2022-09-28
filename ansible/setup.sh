7c0276a5f49a00d0e2df71d0db354e8705405626
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-09-27 23:48:16 -0500

    Add logic for clone

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# TODO: this wont work without an ssh key
+git clone --bare git@github.com:Hacksore/dotfiles.git $HOME/.cfg
+
+alias config="git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@"
+
+config checkout
+
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles 😎";
+else
+    echo "🛑 Please backup/remove these files first!"
+fi;
+
+config checkout
+config config status.showUntrackedFiles no
+
 # installs homebrew to $HOME if not found
 if ! [ -x "$(command -v brew)" ]; then
   cd $HOME

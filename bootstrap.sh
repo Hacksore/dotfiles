96e7d70267b9723aac295d987f46ce262a9d3d7c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-09-29 11:09:10 -0500

    Check dir exists

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# clone the repo with dotfiles
-git clone git@github.com:Hacksore/dotfiles.git "$HOME/dotfiles"
+# clone the repo with dotfiles if not exists
+# TODO: does this work if no ssh-key
+if [ -d "$HOME/dotfiles" ]; then
+  git clone git@github.com:Hacksore/dotfiles.git "$HOME/dotfiles"
+fi
 
 # install the files with stow
 stow .

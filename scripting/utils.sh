5a4af94af744473568d25c379722a9941bcbae6b
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-03-07 11:17:09 -0600

    Add gitclean

--- scripting/utils.sh
+++ scripting/utils.sh
@@ -40,3 +40,16 @@ function timezsh {
   shell=${1-$SHELL}
   for i in $(seq 1 10); do /usr/bin/time $shell -i -c exit; done
 }
+
+function gitclean {
+  # Find all directories that are not ignored by git and store them in an array
+  ignored_dirs=($(git ls-files --others --exclude-standard --directory))
+  # Loop through all directories that are not ignored by git
+  for dir in "${ignored_dirs[@]}"; do
+    # Check if the directory contains any non-gitignored files/folders
+    if [[ -z $(git ls-files --directory "$dir") ]]; then
+      # If the directory contains only gitignored files/folders, remove it
+      rm -r "$dir"
+    fi
+  done
+}

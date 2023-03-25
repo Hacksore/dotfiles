9257abc1ffe45e15808485bb4d39847b7d483249
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-03-25 14:13:42 -0500

    Make plugins better

--- .config/astronvim/lua/user/plugins/init.lua
+++ .config/astronvim/lua/user/plugins/init.lua
@@ -2,12 +2,12 @@ return {
   -- gives me nice highlighting for jsx
   {
     "MaxMEllon/vim-jsx-pretty",
-    event = "User Astrofile"
+    event = "User Astrofile",
   },
   -- add discord presence
   {
     "andweeb/presence.nvim",
-    event = "User Astrofile"
+    event = "User Astrofile",
   },
   -- get tree to shows hidden files
   {
@@ -19,7 +19,7 @@ return {
           hide_dotfiles = false,
           hide_by_name = {
             ".git",
-            ".DS_Store"
+            ".DS_Store",
           },
         },
       },
@@ -27,10 +27,35 @@ return {
   },
   {
     "tpope/vim-surround",
-    event = "User Astrofile"
+    event = "User Astrofile",
   },
+  -- enable github copilot
   {
     "github/copilot.vim",
-    event = "User Astrofile"
-  }
+    event = "User Astrofile",
+  },
+  -- configure the status line to include the mode as well
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      local status = require("astronvim.utils.status")
+      opts.statusline = {
+        hl = { fg = "fg", bg = "bg" },
+        status.component.mode({ mode_text = { padding = { left = 1, right = 1 } } }), -- add the mode text
+        status.component.git_branch(),
+        status.component.file_info({ filetype = {}, filename = false, file_modified = false }),
+        status.component.git_diff(),
+        status.component.diagnostics(),
+        status.component.fill(),
+        status.component.cmd_info(),
+        status.component.fill(),
+        status.component.lsp(),
+        status.component.treesitter(),
+        status.component.nav(),
+      }
+
+      -- return the final configuration table
+      return opts
+    end,
+  },
 }

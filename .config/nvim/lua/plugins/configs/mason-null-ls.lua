9707a7762c37dc9b5a8ad83cc115e87f557a038c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-03-29 09:02:29 -0500

    remove

--- .config/nvim/lua/plugins/configs/mason-null-ls.lua
+++
@@ -1,5 +0,0 @@
-return function(_, opts)
-  local mason_null_ls = require "mason-null-ls"
-  mason_null_ls.setup(opts)
-  mason_null_ls.setup_handlers {}
-end
--- .config/nvim/lua/plugins/configs/mason.lua
+++
@@ -1,6 +0,0 @@
-return function(_, opts)
-  require("mason").setup(opts)
-  for _, plugin in ipairs { "mason-lspconfig", "mason-null-ls", "mason-nvim-dap" } do
-    pcall(require, plugin)
-  end
-end

075ffc76de4480ffc906fdc3fa2d0341df3e3b09
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-04-06 11:14:36 -0500

    Add icon

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -1,46 +1,13 @@
 return {
-	-- add new user interface icon
-	icons = {
-		VimIcon = "",
-		ScrollText = "",
-		GitBranch = "",
-		GitAdd = "",
-		GitChange = "",
-		GitDelete = "",
-	},
-	-- modify variables used by heirline but not defined in the setup call directly
-	heirline = {
-		-- define the separators between each section
-		separators = {
-			left = { "", " " }, -- separator for the left side of the statusline
-			right = { " ", "" }, -- separator for the right side of the statusline
-			tab = { "", "" },
-		},
-		-- add new colors that can be used by heirline
-		colors = function(hl)
-			local get_hlgroup = require("astronvim.utils").get_hlgroup
-			-- use helper function to get highlight group properties
-			local comment_fg = get_hlgroup("Comment").fg
-			hl.git_branch_fg = comment_fg
-			hl.git_added = comment_fg
-			hl.git_changed = comment_fg
-			hl.git_removed = comment_fg
-			hl.blank_bg = get_hlgroup("Folded").fg
-			hl.file_info_bg = get_hlgroup("Visual").bg
-			hl.nav_icon_bg = get_hlgroup("String").fg
-			hl.nav_fg = hl.nav_icon_bg
-			hl.folder_icon_bg = get_hlgroup("Error").fg
-			return hl
-		end,
-		attributes = {
-			mode = { bold = true },
-		},
-		icon_highlights = {
-			file_icon = {
-				statusline = false,
-			},
-		},
-	},
+  -- add new user interface icon
+  icons = {
+    VimIcon = "",
+    ScrollText = "",
+    GitBranch = "",
+    GitAdd = "",
+    GitChange = "",
+    GitDelete = "",
+  },
 	-- any custom config
 	options = {
 		g = {
--- .config/astronvim/lua/user/mappings.lua
+++ .config/astronvim/lua/user/mappings.lua
@@ -3,6 +3,5 @@ return {
 		["<leader>ff"] = { "<cmd>Telescope git_files<cr>", desc = "Telescope git files" },
 		["<leader>1"] = { "<cmd>bprevious<cr>", desc = "Switch buffer backwards" },
 		["<leader>2"] = { "<cmd>bnext<cr>", desc = "Switch buffer forward" },
-		-- ["<C-cr>"] = { "copilot#Accept(<Tab>)", desc = "Accept copi" },
 	},
 }

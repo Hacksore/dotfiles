58dda260ca035fddb17251fe6fdf3d0301edb09c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-05-11 21:46:31 -0500

    Fix todo highlight bug by diabling spell check

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -10,10 +10,10 @@ return {
 	},
 	options = {
 		opt = {
-			spell = true, -- Enable spell checking
+			spell = false, -- Enable spell checking
 			swapfile = false, -- Disable swap files
 			title = true, -- Allow nvim to update the term titlerelativenumber
-			relativenumber = false, -- Diable relative line numbers
+			relativenumber = false, -- Disable relative line numbers
 		},
 	},
 	updater = {
@@ -23,18 +23,6 @@ return {
 		brnch = "main",
 	},
 	lsp = {
-		-- TODO: i need to make this load all the mason and treesitter goodies so I don't have
-		servers = {
-			"tsserver",
-			"cssls",
-			"yamlls",
-			"rust_analyzer",
-			"eslint",
-			"gopls",
-			"html",
-			"jsonls",
-			"lua_ls",
-		},
 		formatting = {
 			format_on_save = {
 				enabled = false, -- disable format on save

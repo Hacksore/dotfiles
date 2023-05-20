71d3c16712db8a5a6e26f42fb7cae91792f10c9d
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-05-19 19:45:24 -0500

    DAP "working"

--- .config/astronvim/lua/user/plugins/dap.lua
+++ .config/astronvim/lua/user/plugins/dap.lua
@@ -1,48 +1,48 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    { "mxsdev/nvim-dap-vscode-js",       opts = { debugger_cmd = { "js-debug-adapter" }, adapters = { "pwa-node" } } },
-    { "theHamsta/nvim-dap-virtual-text", config = true },
-  },
-  config = function()
-    local dap = require("dap")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		{
+			"mxsdev/nvim-dap-vscode-js",
+			opts = { debugger_cmd = { "js-debug-adapter" }, adapters = { "pwa-node" } },
+		},
+		{ "theHamsta/nvim-dap-virtual-text", config = true },
+	},
+	config = function()
+		local dap = require("dap")
+		local port = 9229
+		dap.adapters["pwa-node"] = {
+			type = "server",
+			host = "localhost",
+			-- port = "${port}",
+			port = port,
+			executable = {
+				command = "node",
+				args = {
+					-- TODO: make this not hard coded?
+					"/Users/hacksore/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+					port,
+				},
+			},
+		}
 
-    local attach_node = {
-      type = "pwa-node",
-      request = "attach",
-      name = "Attach",
-      processId = require("dap.utils").pick_process,
-      cwd = "${workspaceFolder}",
-    }
+		local attach_node = {
+			type = "pwa-node",
+			request = "attach",
+			name = "Attach",
+			processId = require("dap.utils").pick_process,
+			cwd = "${workspaceFolder}",
+		}
 
-    dap.configurations.javascript = {
-      {
-        type = "pwa-node",
-        request = "launch",
-        name = "Launch file",
-        program = "${file}",
-        cwd = "${workspaceFolder}",
-      },
-      attach_node,
-    }
-    dap.configurations.typescript = {
-      {
-        type = "pwa-node",
-        request = "launch",
-        name = "Launch file",
-        program = "${file}",
-        cwd = "${workspaceFolder}",
-        runtimeExecutable = "ts-node",
-        sourceMaps = true,
-        protocol = "inspector",
-        console = "integratedTerminal",
-        resolveSourceMapLocations = {
-          "${workspaceFolder}/dist/**/*.js",
-          "${workspaceFolder}/**",
-          "!**/node_modules/**",
-        },
-      },
-      attach_node,
-    }
-  end,
+		-- basic js debug
+		dap.configurations.javascript = {
+			{
+				type = "pwa-node",
+				request = "launch",
+				name = "Launch file",
+				program = "${file}",
+				cwd = "${workspaceFolder}",
+			},
+			attach_node,
+		}
+	end,
 }

543cff54355d98acbf6a04bb68b3c342336b774d
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-05-22 17:20:29 -0500

    Fix the dap for js

--- .config/astronvim/lua/user/plugins/dap.lua
+++ .config/astronvim/lua/user/plugins/dap.lua
@@ -1,48 +1,30 @@
 return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
-		{
-			"mxsdev/nvim-dap-vscode-js",
-			opts = { debugger_cmd = { "js-debug-adapter" }, adapters = { "pwa-node" } },
-		},
 		{ "theHamsta/nvim-dap-virtual-text", config = true },
 	},
 	config = function()
 		local dap = require("dap")
-		local port = 9229
 		dap.adapters["pwa-node"] = {
 			type = "server",
 			host = "localhost",
-			-- port = "${port}",
-			port = port,
+			port = "${port}",
 			executable = {
-				command = "node",
-				args = {
-					-- TODO: make this not hard coded?
-					"/Users/hacksore/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
-					port,
+				command = "js-debug-adapter", -- As I'm using mason, this will be in the path
+				args = { "${port}" },
+			},
+		}
+
+		for _, language in ipairs({ "typescript", "javascript" }) do
+			require("dap").configurations[language] = {
+				{
+					type = "pwa-node",
+					request = "launch",
+					name = "Launch file",
+					program = "${file}",
+					cwd = "${workspaceFolder}",
 				},
-			},
-		}
-
-		local attach_node = {
-			type = "pwa-node",
-			request = "attach",
-			name = "Attach",
-			processId = require("dap.utils").pick_process,
-			cwd = "${workspaceFolder}",
-		}
-
-		-- basic js debug
-		dap.configurations.javascript = {
-			{
-				type = "pwa-node",
-				request = "launch",
-				name = "Launch file",
-				program = "${file}",
-				cwd = "${workspaceFolder}",
-			},
-			attach_node,
-		}
+			}
+		end
 	end,
 }

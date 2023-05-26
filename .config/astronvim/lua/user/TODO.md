b433ab934d84c1e35d682ddb628ed4be5a256475
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-05-26 10:58:44 -0500

    Working TS debug

---
+++ .config/astronvim/lua/user/TODO.md
@@ -0,0 +1,4 @@
+- Fix the eslint issue where a project with no config leaves inlay hit
+- Fix typescript debug
+- Fix javascriptreact and typescriptreact
+
--- .config/astronvim/lua/user/plugins/dap.lua
+++ .config/astronvim/lua/user/plugins/dap.lua
@@ -5,6 +5,8 @@ return {
 	},
 	config = function()
 		local dap = require("dap")
+
+		-- TODO: do we need to have other adapters
 		dap.adapters["pwa-node"] = {
 			type = "server",
 			host = "localhost",
@@ -15,16 +17,61 @@ return {
 			},
 		}
 
-		for _, language in ipairs({ "typescript", "javascript" }) do
-			require("dap").configurations[language] = {
+		for _, language in ipairs({ "javascript" }) do
+			dap.configurations[language] = {
 				{
 					type = "pwa-node",
 					request = "launch",
-					name = "Launch file",
+					name = "Launch File",
 					program = "${file}",
+					cwd = "${workspaceFolder}",
+				},
+				{
+					type = "pwa-node",
+					request = "launch",
+					name = "Attach to Process",
+					proccessId = require("dap.utils").pick_process,
 					cwd = "${workspaceFolder}",
 				},
 			}
 		end
+
+		dap.configurations["typescript"] = {
+			-- NOTE: this is working but only allows you to run the current file
+			-- you also need to have both ts-node and typescript installed as dev deps
+			{
+				type = "pwa-node",
+				request = "launch",
+				name = "Launch Current File (pwa-node with ts-node)",
+				cwd = vim.fn.getcwd(),
+				runtimeArgs = { "--loader", "ts-node/esm" },
+				runtimeExecutable = "node",
+				args = { "${file}" },
+				sourceMaps = true,
+				protocol = "inspector",
+				skipFiles = { "<node_internals>/**", "node_modules/**" },
+				resolveSourceMapLocations = {
+					"${workspaceFolder}/**",
+					"!**/node_modules/**",
+				},
+			},
+			-- TODO: how can I make it allow for debughing npm scripts?
+			-- {
+			-- 	type = "pwa-node",
+			-- 	request = "launch",
+			-- 	name = "Launch Current File (pwa-node with ts-node)",
+			-- 	cwd = vim.fn.getcwd(),
+			-- 	runtimeArgs = { "--loader", "ts-node/esm" },
+			-- 	runtimeExecutable = "node",
+			-- 	args = { "${file}" },
+			-- 	sourceMaps = true,
+			-- 	protocol = "inspector",
+			-- 	skipFiles = { "<node_internals>/**", "node_modules/**" },
+			-- 	resolveSourceMapLocations = {
+			-- 		"${workspaceFolder}/**",
+			-- 		"!**/node_modules/**",
+			-- 	},
+			-- },
+		}
 	end,
 }

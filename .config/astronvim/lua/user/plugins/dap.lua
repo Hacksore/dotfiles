7a71a3bcbe4a587f80c0fe5c60844bab79ef0562
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-05-27 12:25:20 -0500

    Dap with TS/JS working

--- .config/astronvim/lua/user/plugins/dap.lua
+++ .config/astronvim/lua/user/plugins/dap.lua
@@ -6,72 +6,104 @@ return {
 	config = function()
 		local dap = require("dap")
 
-		-- TODO: do we need to have other adapters
+		local masonPath = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
+		local jsDebugBin = masonPath .. "bin/js-debug-adapter"
+
+		dap.adapters.chrome = {
+			type = "executable",
+			command = "node",
+			args = {
+				masonPath .. "packages/chrome-debug-adapter/out/src/chromeDebug.js",
+			},
+		}
+
+		-- This is what vscode uses and i cant get it working yet
+		-- https://nextjs.org/docs/pages/building-your-application/configuring/debugging
+		dap.adapters["node-terminal"] = {
+			type = "executable",
+			command = "node",
+			-- args = { testP },
+		}
+
 		dap.adapters["pwa-node"] = {
 			type = "server",
 			host = "localhost",
 			port = "${port}",
-			executable = {
-				command = "js-debug-adapter", -- As I'm using mason, this will be in the path
-				args = { "${port}" },
-			},
+			executable = { command = jsDebugBin, args = { "${port}" } },
 		}
 
-		for _, language in ipairs({ "javascript" }) do
-			dap.configurations[language] = {
-				{
-					type = "pwa-node",
-					request = "launch",
-					name = "Launch File",
-					program = "${file}",
-					cwd = "${workspaceFolder}",
-				},
-				{
-					type = "pwa-node",
-					request = "launch",
-					name = "Attach to Process",
-					proccessId = require("dap.utils").pick_process,
-					cwd = "${workspaceFolder}",
+		local pwaNodeAttach = {
+			type = "pwa-node",
+			request = "launch",
+			name = "js-debug: Attach to Process (pwa-node)",
+			proccessId = require("dap.utils").pick_process,
+			cwd = "${workspaceFolder}",
+		}
+
+		local function typescript(args)
+			return {
+				type = "pwa-node",
+				request = "launch",
+				name = ("js-debug: Launch Current File (ts-node%s)"):format(
+					args and (" " .. table.concat(args, " ")) or ""
+				),
+				program = "${file}",
+				cwd = "${workspaceFolder}",
+				runtimeExecutable = "ts-node",
+				runtimeArgs = args,
+				sourceMaps = true,
+				protocol = "inspector",
+				console = "integratedTerminal",
+				resolveSourceMapLocations = {
+					"${workspaceFolder}/dist/**/*.js",
+					"${workspaceFolder}/**",
+					"!**/node_modules/**",
 				},
 			}
 		end
 
-		dap.configurations["typescript"] = {
-			-- NOTE: this is working but only allows you to run the current file
-			-- you also need to have both ts-node and typescript installed as dev deps
-			{
-				type = "pwa-node",
-				request = "launch",
-				name = "Launch Current File (pwa-node with ts-node)",
-				cwd = vim.fn.getcwd(),
-				runtimeArgs = { "--loader", "ts-node/esm" },
-				runtimeExecutable = "node",
-				args = { "${file}" },
-				sourceMaps = true,
-				protocol = "inspector",
-				skipFiles = { "<node_internals>/**", "node_modules/**" },
-				resolveSourceMapLocations = {
-					"${workspaceFolder}/**",
-					"!**/node_modules/**",
+		for _, language in ipairs({ "javascript", "javascriptreact" }) do
+			dap.configurations[language] = {
+				{
+					type = "pwa-node",
+					request = "launch",
+					name = "js-debug: Launch File (pwa-node)",
+					program = "${file}",
+					cwd = "${workspaceFolder}",
 				},
-			},
-			-- TODO: how can I make it allow for debughing npm scripts?
-			-- {
-			-- 	type = "pwa-node",
-			-- 	request = "launch",
-			-- 	name = "Launch Current File (pwa-node with ts-node)",
-			-- 	cwd = vim.fn.getcwd(),
-			-- 	runtimeArgs = { "--loader", "ts-node/esm" },
-			-- 	runtimeExecutable = "node",
-			-- 	args = { "${file}" },
-			-- 	sourceMaps = true,
-			-- 	protocol = "inspector",
-			-- 	skipFiles = { "<node_internals>/**", "node_modules/**" },
-			-- 	resolveSourceMapLocations = {
-			-- 		"${workspaceFolder}/**",
-			-- 		"!**/node_modules/**",
-			-- 	},
-			-- },
-		}
+				pwaNodeAttach,
+			}
+		end
+
+		for _, language in ipairs({ "typescript", "typescriptreact" }) do
+			dap.configurations[language] = {
+				typescript(),
+				typescript({ "--esm" }),
+				-- NOTE: testing the nodejs debug setup and works for API and initial SSR
+				{
+					type = "pwa-node",
+					name = "Next.js: debug server",
+					request = "launch",
+					program = "${workspaceFolder}/node_modules/next/dist/bin/next",
+					args = { "dev" },
+					sourceMaps = true,
+					cwd = "${workspaceFolder}",
+					runtimeArgs = { "--inspect" },
+					skipFiles = { "${workspaceFolder}/node_modules/**/*.js", "<node_internals>/**/*.js" },
+					console = "integratedTerminal",
+					protocol = "inspector",
+				},
+				{
+					name = "Next.Js: debug client",
+					type = "chrome",
+					request = "launch",
+					url = "http://localhost:3000",
+					webRoot = "${workspaceFolder}",
+					runtimeExecutable = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+					userDataDir = "/tmp/chrome-dev",
+				},
+				pwaNodeAttach,
+			}
+		end
 	end,
 }
--- scripting/aliases.sh
+++ scripting/aliases.sh
@@ -6,7 +6,9 @@ alias gs="git status"
 alias ga="git add"
 alias gc="git commit"
 alias gp="git push"
-alias v="code"
+alias v="nvim"
+alias vim="nvim"
+alias vi="nvim"
 
 # reload the shell to source - might be better to use source command instead to save history
 alias s="zsh"

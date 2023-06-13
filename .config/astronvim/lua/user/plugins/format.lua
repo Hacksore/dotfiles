80e5fa8c0f91507e4ac85ef960a2a3b2426e1c0b
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-06-13 17:34:20 -0500

    Make sure LSP installed

--- .config/astronvim/lua/user/plugins/format.lua
+++ .config/astronvim/lua/user/plugins/format.lua
@@ -2,6 +2,17 @@ return {
   "jay-babu/mason-null-ls.nvim",
   opts = {
     handlers = {
+      eslint_d = function()
+        local nullLs = require("null-ls")
+        nullLs.register(nullLs.builtins.diagnostics.eslint_d.with({
+          condition = function(utils)
+            return utils.root_has_file(".eslintrc") or
+            utils.root_has_file(".eslintrc.json") or
+            utils.root_has_file(".eslintrc.mjs") or
+            utils.root_has_file(".eslintrc.cjs")
+          end,
+        }))
+      end,
       -- for prettier
       prettier = function()
         local nullLs = require("null-ls")
---
+++ .config/astronvim/lua/user/plugins/mason.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "bashls",
+        "lua_ls",
+        "cssls",
+        "dockerls",
+        "html",
+        "jsonls",
+        "rust_analyzer",
+        "tsserver",
+        "voler",
+        "yamlls",
+      },
+    },
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint_d",
+        "prettier",
+      },
+    },
+  }
+}

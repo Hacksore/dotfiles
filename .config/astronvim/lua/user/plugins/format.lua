08840378aaf1f5d619b47b8ce731486ccebbd4b8
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-06-13 18:09:58 -0500

    Fix eslint Deez

--- .config/astronvim/lua/user/plugins/format.lua
+++ .config/astronvim/lua/user/plugins/format.lua
@@ -1,30 +1,27 @@
 return {
-  "jay-babu/mason-null-ls.nvim",
-  opts = {
-    handlers = {
-      eslint_d = function()
-        local nullLs = require("null-ls")
-        nullLs.register(nullLs.builtins.diagnostics.eslint_d.with({
-          condition = function(utils)
-            return utils.root_has_file(".eslintrc") or
-            utils.root_has_file(".eslintrc.json") or
-            utils.root_has_file(".eslintrc.mjs") or
-            utils.root_has_file(".eslintrc.cjs")
-          end,
-        }))
-      end,
-      -- for prettier
-      prettier = function()
-        local nullLs = require("null-ls")
-        nullLs.register(nullLs.builtins.formatting.prettier.with({
-          condition = function(utils)
-            return utils.root_has_file("package.json")
-                or utils.root_has_file(".prettierrc")
-                or utils.root_has_file(".prettierrc.json")
-                or utils.root_has_file(".prettierrc.js")
-          end,
-        }))
-      end,
-    },
-  },
+	"jay-babu/mason-null-ls.nvim",
+	opts = {
+		handlers = {
+			eslint_d = function()
+				local nullLs = require("null-ls")
+				nullLs.register(nullLs.builtins.diagnostics.eslint_d.with({
+					condition = function(utils)
+						return utils.root_has_file(".eslintrc.json") or utils.root_has_file(".eslintrc.js")
+					end,
+				}))
+			end,
+			-- for prettier
+			prettier = function()
+				local nullLs = require("null-ls")
+				nullLs.register(nullLs.builtins.formatting.prettier.with({
+					condition = function(utils)
+						return utils.root_has_file("package.json")
+							or utils.root_has_file(".prettierrc")
+							or utils.root_has_file(".prettierrc.json")
+							or utils.root_has_file(".prettierrc.js")
+					end,
+				}))
+			end,
+		},
+	},
 }

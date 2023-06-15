2fb1d3cee03d9dd39eee5ff6636e147316386b53
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-06-15 00:36:58 +0000

    Add treesitter grammers

---
+++ .config/astronvim/lua/user/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "css",
+      "dockerfile",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "python",
+      "rust",
+      "toml",
+      "typescript",
+      "tsx",
+      "diff",
+      "ini",
+      "astro",
+      "vue",
+      "yaml"
+    }
+  }
+}

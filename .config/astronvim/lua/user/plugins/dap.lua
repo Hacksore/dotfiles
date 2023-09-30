c97779c7e7aa05caed033dbb113a30e9b47a3605
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-09-30 09:54:20 -0500

    Dapping again

--- .config/astronvim/lua/user/plugins/dap.lua
+++ .config/astronvim/lua/user/plugins/dap.lua
@@ -17,12 +17,10 @@ return {
 			},
 		}
 
-		-- This is what vscode uses and i cant get it working yet
 		-- https://nextjs.org/docs/pages/building-your-application/configuring/debugging
 		dap.adapters["node-terminal"] = {
 			type = "executable",
 			command = "node",
-			-- args = { testP },
 		}
 
 		dap.adapters["pwa-node"] = {
@@ -40,26 +38,7 @@ return {
 			cwd = "${workspaceFolder}",
 		}
 
-		local function typescript(args)
-			return {
-				type = "pwa-node",
-				request = "launch",
-				name = "js-debug: Launch Current File (tsx%s)",
-				program = "${file}",
-				cwd = "${workspaceFolder}",
-				runtimeExecutable = "node",
-				runtimeArgs = {"--loader", "tsx"},
-				sourceMaps = true,
-				protocol = "inspector",
-				console = "integratedTerminal",
-				resolveSourceMapLocations = {
-					"${workspaceFolder}/dist/**/*.js",
-					"${workspaceFolder}/**",
-					"!**/node_modules/**",
-				},
-			}
-		end
-
+		-- debug nodejs scripts
 		for _, language in ipairs({ "javascript", "javascriptreact" }) do
 			dap.configurations[language] = {
 				{
@@ -75,42 +54,22 @@ return {
 
 		for _, language in ipairs({ "typescript", "typescriptreact" }) do
 			dap.configurations[language] = {
-				typescript(),
-				-- typescript({ "--esm" }),
-				-- NOTE: testing the nodejs debug setup and works for API and initial SSR
 				{
 					type = "pwa-node",
-					name = "Next.js: debug server",
 					request = "launch",
-					program = "${workspaceFolder}/node_modules/next/dist/bin/next",
-					args = { "dev" },
+					name = "typescript debug: Launch Current File (tsx)",
+					program = "${file}",
+					cwd = "${workspaceFolder}",
+					runtimeExecutable = "node",
+					runtimeArgs = { "--loader", "tsx" },
 					sourceMaps = true,
-					cwd = "${workspaceFolder}",
-					runtimeArgs = { "--inspect" },
-					skipFiles = { "${workspaceFolder}/node_modules/**/*.js", "<node_internals>/**/*.js" },
-					console = "integratedTerminal",
 					protocol = "inspector",
-				},
-				{
-					type = "pwa-node",
-					name = "debug: npm start",
-					request = "launch",
-					program = "npm",
-					args = { "start" },
-					cwd = "${workspaceFolder}",
-					runtimeArgs = { "--inspect" },
-					skipFiles = { "${workspaceFolder}/node_modules/**/*.js", "<node_internals>/**/*.js" },
 					console = "integratedTerminal",
-					protocol = "inspector",
-				},
-				{
-					name = "Next.Js: debug client",
-					type = "chrome",
-					request = "launch",
-					url = "http://localhost:3000",
-					webRoot = "${workspaceFolder}",
-					runtimeExecutable = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-					userDataDir = "/tmp/chrome-dev",
+					resolveSourceMapLocations = {
+						"${workspaceFolder}/dist/**/*.js",
+						"${workspaceFolder}/**",
+						"!**/node_modules/**",
+					},
 				},
 				pwaNodeAttach,
 			}

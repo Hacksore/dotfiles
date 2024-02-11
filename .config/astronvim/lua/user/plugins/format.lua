a0c62c6ed2ecad6f53503b437b901ff3bc267276
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-02-10 20:59:26 -0600

    Prettier mono check

--- .config/astronvim/lua/user/plugins/format.lua
+++ .config/astronvim/lua/user/plugins/format.lua
@@ -2,6 +2,24 @@ return {
   "jay-babu/mason-null-ls.nvim",
   opts = {
     handlers = {
+      -- for prettier
+      prettier = function()
+        local nullLs = require("null-ls")
+        nullLs.register(nullLs.builtins.formatting.prettier.with({
+          condition = function(utils)
+            local files = {
+              "package.json",
+              ".prettierrc",
+              ".prettierrc.json",
+              ".prettierrc.js",
+              "prettier.config.js",
+              "prettier.config.cjs",
+              ".prettierrc.cjs",
+            }
+            return utils.root_has_file(files)
+          end,
+        }))
+      end,
       beautysh = function()
         local nullLs = require("null-ls")
         nullLs.register(nullLs.builtins.formatting.beautysh.with({

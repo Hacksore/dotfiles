2c6a524dfff8eaf6dcbb5c98de435882c1009aae
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-02-13 10:48:18 -0600

    Thaks @creativenull ❤️

--- .config/astronvim/lua/user/plugins/init.lua
+++ .config/astronvim/lua/user/plugins/init.lua
@@ -1,4 +1,26 @@
 return {
+
+	{
+		"creativenull/dotfyle-metadata.nvim",
+		config = function()
+			local augroup = vim.api.nvim_create_augroup("DotfyleMetadataGroup", {})
+			vim.api.nvim_create_autocmd("User", {
+				group = augroup,
+				pattern = { "LazyUpdate", "LazyInstall", "LazySync", "LazyClean" },
+				once = true,
+				callback = function()
+					local dotfyle_metadata = require("dotfyle_metadata")
+					dotfyle_metadata.generate({})
+					local home = vim.fn.getenv("HOME")
+					vim.cmd("!npx prettier --write " .. dotfyle_metadata.dotfyle_path)
+					local move_dir_string = string.format("!mv %s %s", dotfyle_metadata.dotfyle_path,
+						home .. "/.config/astronvim/lua/user")
+					vim.cmd(move_dir_string)
+				end,
+			})
+		end,
+		event = "VeryLazy",
+	},
 	-- gives me nice highlighting for jsx
 	{
 		"MaxMEllon/vim-jsx-pretty",

29f922a4ceb7f749b703812bd13bd642725584e2
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-04-01 21:19:40 -0500

    remove commentws

--- .config/nvim/lua/plugins/astrolsp.lua
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -13,22 +13,11 @@ return {
 			format_on_save = {
 				enabled = false,
 			},
-			timeout_ms = 1000, -- default format timeout
-			-- filter = function(client) -- fully override the default formatting function
-			--   return true
-			-- end
+			timeout_ms = 1000
 		},
-		-- Configure buffer local auto commands to add when attaching a language server
 		autocmds = {
-			-- first key is the `augroup` to add the auto commands to (:h augroup)
 			lsp_document_highlight = {
-				-- Optional condition to create/delete auto command group
-				-- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
-				-- condition will be resolved for each client on each execution and if it ever fails for all clients,
-				-- the auto commands will be deleted for that buffer
 				cond = "textDocument/documentHighlight",
-				-- cond = function(client, bufnr) return client.name == "lua_ls" end,
-				-- list of auto commands to set
 				{
 					-- events to trigger
 					event = { "CursorHold", "CursorHoldI" },

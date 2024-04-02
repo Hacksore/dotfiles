e11ce3fff5211cb2eb76b70a9d6e9aeb5eb68e6c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-04-02 12:17:18 -0500

    Ignore is the way

--- .config/nvim/lua/plugins/astrolsp.lua
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -13,39 +13,22 @@ return {
 			format_on_save = {
 				enabled = false,
 			},
-			timeout_ms = 1000
+			timeout_ms = 1000,
 		},
-		autocmds = {
-			lsp_document_highlight = {
-				cond = "textDocument/documentHighlight",
-				{
-					-- events to trigger
-					event = { "CursorHold", "CursorHoldI" },
-					-- the rest of the autocmd options (:h nvim_create_autocmd)
-					desc = "Document Highlighting",
-					callback = function()
-						vim.lsp.buf.document_highlight()
-					end,
-				},
-				{
-					event = { "CursorMoved", "CursorMovedI", "BufLeave" },
-					desc = "Document Highlighting Clear",
-					callback = function()
-						vim.lsp.buf.clear_references()
-					end,
-				},
+    ---@diagnostic disable-next-line: missing-fields
+		config = {
+    	---@diagnostic disable-next-line: missing-fields
+			tailwindcss = {
+				enabled = true,
+				on_attach = function(client, bufnr)
+					local tw_highlight = require("tailwind-highlight")
+					tw_highlight.setup(client, bufnr, {
+						single_column = false,
+						mode = "background",
+						debounce = 200,
+					})
+				end,
 			},
 		},
-		on_attach = function(client, bufnr)
-			-- only do when tailwind lsp attached
-			if client.name == "tailwindcss" then
-				local tw_highlight = require("tailwind-highlight")
-				tw_highlight.setup(client, bufnr, {
-					single_column = false,
-					mode = "background",
-					debounce = 200,
-				})
-			end
-		end,
 	},
 }

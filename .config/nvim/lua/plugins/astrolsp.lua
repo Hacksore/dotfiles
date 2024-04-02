ed02f49f3dcd9b9edcc399f26c97b0295bcbc198
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-04-02 08:15:21 -0500

    Move keymappings to core

--- .config/nvim/lua/plugins/astrolsp.lua
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -36,47 +36,6 @@ return {
 				},
 			},
 		},
-		-- mappings to be set up on attaching of a language server
-		mappings = {
-			v = {
-				-- stop making the pasted text go into my clipboard buffer
-				p = {
-					[["_dP]], -- this goes to narnia (aka the void)
-				},
-			},
-			n = {
-				["<leader>ff"] = { "<cmd>Telescope find_files hidden=true<cr>", desc = "Telescope find files" },
-				["<leader>gg"] = { "<cmd>silent !tmux neww lazygit<cr>", desc = "Open lazygit" },
-				["<leader>1"] = { "<cmd>bprevious<cr>", desc = "Switch buffer backwards" },
-				["<leader>2"] = { "<cmd>bnext<cr>", desc = "Switch buffer forward" },
-				["<leader>h"] = false,
-				-- NOTE: this is a bunch of stuff for tmux
-				["<C-n>"] = {
-					function()
-						vim.fn.system("tmux switch-client -n")
-					end,
-					desc = "Switch tmux session forward",
-				},
-				["<C-p>"] = {
-					function()
-						vim.fn.system("tmux switch-client -p")
-					end,
-					desc = "Switch tmux session back",
-				},
-				-- open the fuzzy menu for finding projects
-				["<C-f>"] = { "<cmd>silent !tmux neww tmux-sessionizer<cr>", desc = "Open tmux sessionizer" },
-				-- test auto import
-				["<leader>ll"] = {
-					function()
-						vim.lsp.buf.code_action({
-							context = { only = { "source.addMissingImports" }, triggerKind = 1 },
-							apply = true,
-						})
-					end,
-					desc = "test null code",
-				},
-			},
-		},
 		on_attach = function(client, bufnr)
 			-- only do when tailwind lsp attached
 			if client.name == "tailwindcss" then

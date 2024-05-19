e9731fd4c84000de73f2fc00eb9b9c0ff8c10367
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-05-18 20:39:57 -0500

    Fix copliot hotkey bug

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"zbirenbaum/copilot.lua",
 		cmd = "Copilot",
-		event = "InsertEnter",
+		-- event = "InsertEnter",
 		config = function()
 			require("copilot").setup({
 				suggestion = {

9cf9e36cd27ff2548f62281bc8998e913c7b93b8
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-17 16:23:27 -0500

    Yaml formatting 🥳

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -3,15 +3,15 @@ return {
 	"nvimtools/none-ls.nvim",
 	opts = function(_, config)
 		-- config variable is the default configuration table for the setup function call
-		-- local null_ls = require "null-ls"
+		local null_ls = require "null-ls"
 
 		-- Check supported formatters and linters
 		-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-		-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 		config.sources = {
-			-- Set a formatter
-			-- null_ls.builtins.formatting.stylua,
-			-- null_ls.builtins.formatting.prettier,
+			null_ls.builtins.formatting.stylua,
+			null_ls.builtins.formatting.prettier,
+			-- yaml formatting 🥳
+			null_ls.builtins.formatting.yamlfmt,
 		}
 		return config -- return final config table
 	end,

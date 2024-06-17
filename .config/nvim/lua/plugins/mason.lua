894df25aafbd6bb55587905288a6b01aa6a94b5b
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-17 16:36:00 -0500

    Use yamlfmt

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -39,7 +39,7 @@ return {
 			-- add more things to the ensure_installed table protecting against community packs modifying it
 			opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
 				"prettier",
-				"yamlfix",
+				"yamlfmt",
 				"shfmt",
 			})
 		end,

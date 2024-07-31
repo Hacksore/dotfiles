2783a0020d499c089b8778294be013943e5990e2
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-07-31 06:22:10 -0500

    Use ts pack and add todo

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -1,5 +1,7 @@
+--- https://github.com/AstroNvim/astrocommunity
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/typescript
   { import = "astrocommunity.pack.typescript" },
 }
---
+++ TODO.md
@@ -0,0 +1,6 @@
+Fix these
+- format on save in typescript files (caused by pack?)
+- files the have strange amounts of JSX will break prettier fomatting
+- control+o/i should reopen a closed buffer
+- 
+

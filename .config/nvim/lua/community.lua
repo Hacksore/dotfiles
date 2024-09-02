a2a493a0ef3238c7d63193559e936a050c1ef141
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-02 18:52:45 -0500

    Update deps

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -1,9 +1,7 @@
 --- https://github.com/AstroNvim/astrocommunity
 ---@type LazySpec
-return {}
--- NOTE: disable this pack to fix the auto save issue for now
--- return {
---   -- "AstroNvim/astrocommunity",
---   -- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/typescript
---   -- { import = "astrocommunity.pack.typescript" },
--- }
+return {
+ "AstroNvim/astrocommunity",
+  -- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/typescript
+  { import = "astrocommunity.pack.typescript" },
+}

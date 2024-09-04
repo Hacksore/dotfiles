a9bb6279e7621631cbb1dca03779b2a05761c454
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-04 18:09:50 -0500

    neo-tree bindings

--- .config/nvim/lua/hacksore/core/keymaps.lua
+++ .config/nvim/lua/hacksore/core/keymaps.lua
@@ -4,3 +4,14 @@ local keymap = vim.keymap -- for conciseness
 
 keymap.set("n", "<Leader>ff", "<cmd>Telescope find_files hidden=true<cr>", { desc = "Telescope find files" })
 keymap.set("n", "<Leader>gg", "<cmd>silent !tmux neww lazygit<cr>", { desc = "Open lazygit in tmux" })
+
+keymap.set("n", "<Leader>e", "<Cmd>Neotree toggle<CR>", { desc = "Toggle Explorer" })
+keymap.set("n", "<Leader>o", function()
+	if vim.bo.filetype == "neo-tree" then
+		vim.cmd.wincmd("p")
+	else
+		vim.cmd.Neotree("focus")
+	end
+end, {
+	desc = "Toggle Explorer Focus",
+})
--- .config/nvim/lua/hacksore/plugins/neo-tree.lua
+++ .config/nvim/lua/hacksore/plugins/neo-tree.lua
@@ -44,18 +44,5 @@ return {
 		source_selector = {
 			winbar = false,
 		},
-		mappings = {
-			["<Leader>e"] = { "<Cmd>Neotree toggle<CR>", desc = "Toggle Explorer" },
-			["<Leader>o"] = {
-				function()
-					if vim.bo.filetype == "neo-tree" then
-						vim.cmd.wincmd("p")
-					else
-						vim.cmd.Neotree("focus")
-					end
-				end,
-				desc = "Toggle Explorer Focus",
-			},
-		},
 	},
 }

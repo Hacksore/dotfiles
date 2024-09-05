3ed96e86f5a647cadee47b4ad6fdb6af9879137e
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-04 19:32:58 -0500

    cmp meny

--- .config/nvim/lua/hacksore/plugins/nvim-cmp.lua
+++ .config/nvim/lua/hacksore/plugins/nvim-cmp.lua
@@ -1,3 +1,12 @@
+local function has_words_before()
+	local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local function is_visible(cmp)
+	return cmp.core.view:visible() or vim.fn.pumvisible() == 1
+end
+
 return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
@@ -35,13 +44,41 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if is_visible(cmp) then
+						cmp.select_next_item()
+					elseif
+						vim.api.nvim_get_mode().mode ~= "c"
+						and vim.snippet
+						and vim.snippet.active({ direction = 1 })
+					then
+						vim.schedule(function()
+							vim.snippet.jump(1)
+						end)
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if is_visible(cmp) then
+						cmp.select_prev_item()
+					elseif
+						vim.api.nvim_get_mode().mode ~= "c"
+						and vim.snippet
+						and vim.snippet.active({ direction = -1 })
+					then
+						vim.schedule(function()
+							vim.snippet.jump(-1)
+						end)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({

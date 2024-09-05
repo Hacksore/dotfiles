b7b163f4461067790ae8bdb28c3b5a9b22ef65a0
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-04 20:20:14 -0500

    Remove delay

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,6 +7,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Hightlight selection on yank",
 	pattern = "*",
 	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+		vim.highlight.on_yank({ higroup = "IncSearch" })
 	end,
 })

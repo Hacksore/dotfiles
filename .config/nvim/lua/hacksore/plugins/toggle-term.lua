d3760a9d58d8d459b3846e9c9d02f3a6ccb5ebec
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-04 20:02:00 -0500

    Term colors

--- .config/nvim/lua/hacksore/plugins/toggle-term.lua
+++ .config/nvim/lua/hacksore/plugins/toggle-term.lua
@@ -1,3 +1,19 @@
 return {
-	{ "akinsho/toggleterm.nvim", version = "*", opts = {} },
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {
+			highlights = {
+				Normal = { link = "Normal" },
+				NormalNC = { link = "NormalNC" },
+				NormalFloat = { link = "NormalFloat" },
+				FloatBorder = { link = "FloatBorder" },
+				StatusLine = { link = "StatusLine" },
+				StatusLineNC = { link = "StatusLineNC" },
+				WinBar = { link = "WinBar" },
+				WinBarNC = { link = "WinBarNC" },
+			},
+			size = 10,
+		},
+	},
 }

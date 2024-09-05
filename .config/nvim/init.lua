a3ad1d8ce1cd9dfb83c6b28a96e49ca034accb48
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-04 20:18:37 -0500

    No way yank highlight is not built in

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,12 @@
 require("hacksore.core")
 require("hacksore.lazy")
+
+-- WELP
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", {}),
+	desc = "Hightlight selection on yank",
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+	end,
+})

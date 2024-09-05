48922a8318ee8893e2175665c7c827c7b0528d72
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-05 17:50:28 -0500

    Moar updates

--- .config/nvim/lua/hacksore/plugins/toggleterm.lua
+++ .config/nvim/lua/hacksore/plugins/toggleterm.lua
@@ -1,19 +1,22 @@
 return {
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		opts = {
-			highlights = {
-				Normal = { link = "Normal" },
-				NormalNC = { link = "NormalNC" },
-				NormalFloat = { link = "NormalFloat" },
-				FloatBorder = { link = "FloatBorder" },
-				StatusLine = { link = "StatusLine" },
-				StatusLineNC = { link = "StatusLineNC" },
-				WinBar = { link = "WinBar" },
-				WinBarNC = { link = "WinBarNC" },
-			},
-			size = 10,
-		},
-	},
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      float_opts = {
+        border = "curved"
+      },
+      highlights = {
+        Normal = { link = "Normal" },
+        NormalNC = { link = "NormalNC" },
+        NormalFloat = { link = "NormalFloat" },
+        FloatBorder = { link = "FloatBorder" },
+        StatusLine = { link = "StatusLine" },
+        StatusLineNC = { link = "StatusLineNC" },
+        WinBar = { link = "WinBar" },
+        WinBarNC = { link = "WinBarNC" },
+      },
+      size = 10,
+    },
+  },
 }
--- TODO.md
+++ TODO.md
@@ -6,3 +6,5 @@ action items
 - toggleterm should open where neotree is opened at
 - need to have a <leader>gd for diff copypasta
 - neotree should open dirs with "o" or shift+o
+- create new bufffer split commands
+- prollly need some perf guards for large files on a single line to not kill tree sitter or an lsp

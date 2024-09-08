ac2372ef3b0a5eb0659205cf07aee53c6abdf9b9
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-07 20:14:32 -0500

    Housekeeping

---
+++ .config/nvim/lua/hacksore/plugins/diffview.lua
@@ -0,0 +1,22 @@
+return {
+  "sindrets/diffview.nvim",
+  opts = function()
+    return {
+      keymaps = {
+        disable_defaults = false, -- Disable the default keymaps
+        file_panel = {
+          { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Close diffview' } },
+        },
+        file_history_panel = {
+          { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Close diffview' } },
+        },
+      },
+    }
+  end,
+  keys = {
+    { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = 'DiffviewFileHistory %' },
+    { '<leader>gH', '<cmd>DiffviewFileHistory<cr>', desc = 'DiffviewFileHistory' },
+    { '<leader>gc', '<cmd>Telescope git_commits<cr>', desc = 'Checkout commit' },
+    { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'DiffviewOpen' },
+  },
+}
--- TODO.md
+++ TODO.md
@@ -4,9 +4,7 @@ action items
 
 - toggleterm colors are awful 🤮
 - toggleterm should open where neotree is opened at
-- need to have a <leader>gd for diff copypasta
 - rust should scan for a rustfmt.toml so that it can intepret
-- add fugitive for good blame
 - create new bufffer split commands
 - prollly need some perf guards for large files on a single line to not kill tree sitter or an lsp
 

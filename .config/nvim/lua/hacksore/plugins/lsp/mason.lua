37b0a7794ba2e5d29e69bb49b76b57cd4cb2bf6f
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-10-10 20:36:12 -0500

    Update LSP

--- .config/nvim/lua/hacksore/plugins/lsp/mason.lua
+++ .config/nvim/lua/hacksore/plugins/lsp/mason.lua
@@ -27,7 +27,7 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
+        "ts_ls",
         "html",
         "cssls",
         "tailwindcss",
@@ -38,7 +38,7 @@ return {
         "jsonls",
         "docker_compose_language_service",
         "biome",
-        "astro-language-server",
+        "astro",
         "yamlls",
         "graphql",
         "prismals",

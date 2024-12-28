ce3a5f2292f3f9cc093b6ac9c3e6d58f31f4ecf2
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-12-28 14:43:25 -0600

    Oppsie woopsie

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -79,6 +79,13 @@ local function center_window_once(window)
   wezterm.GLOBAL.windows_centered = wezterm.GLOBAL.windows_centered or {}
 
   local window_id = window:window_id() .. ""
+
+  -- NOTE: this will make sure to only center the window once but has some bugs
+  -- where if you spam it it will not center the windows after some amount of spawned windows
+  if wezterm.GLOBAL.windows_centered[window_id] then
+    return
+  end
+
   local screen = wezterm.gui.screens().active
 
   local width = screen.width * 0.85

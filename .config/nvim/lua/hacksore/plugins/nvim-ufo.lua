e44760068e93fca84c8ff3390b8185b48ad2fdb9
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-01-01 15:50:23 -0600

    Add folding

---
+++ .config/nvim/lua/hacksore/plugins/nvim-ufo.lua
@@ -0,0 +1,20 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  event = "VeryLazy",
+  dependencies = {
+    "kevinhwang91/promise-async"
+  },
+  opts = {
+    provider_selector = function()
+      return { "treesitter", "indent" }
+    end,
+    open_fold_hl_timeout = 400,
+  },
+  init = function()
+    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+    vim.o.foldcolumn = "1"    -- show foldcolumn
+    vim.o.foldenable = true   -- enable fold for nvim-ufo
+    vim.o.foldlevel = 99      -- set high foldlevel for nvim-ufo
+    vim.o.foldlevelstart = 99 -- start with all code unfolded
+  end,
+}
---
+++ .config/nvim/lua/hacksore/plugins/statuscol.lua
@@ -0,0 +1,26 @@
+return {
+  "luukvbaal/statuscol.nvim",
+  config = function()
+    local builtin = require("statuscol.builtin")
+    require("statuscol").setup({
+      -- configuration goes here, for example:
+      relculright = true,
+      segments = {
+        -- Show the number column
+        { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+        -- Show git status
+        {
+          sign = {
+            namespace = { "gitsign" },
+            maxwidth = 1,
+            colwidth = 1,
+            auto = false,
+          },
+          click = "v:lua.ScSa",
+        },
+        -- Padding
+        { text = { " " } },
+      }
+    })
+  end,
+}

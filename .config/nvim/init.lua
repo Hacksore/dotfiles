32f8f1313c3dd3184204dcd2342e4f72007881aa
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-01-01 15:35:16 -0600

    Lazygit good lookin™

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,10 +3,12 @@ require("hacksore.lazy")
 
 -- NOTE: allows for yank highlight
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("highlight_yank", {}),
-	desc = "Hightlight selection on yank",
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch" })
-	end,
+  group = vim.api.nvim_create_augroup("highlight_yank", {}),
+  desc = "Hightlight selection on yank",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch" })
+  end,
 })
+
+vim.api.nvim_set_hl(0, "LazyGitBorder", { fg = "#0D1117", bg = "none" })

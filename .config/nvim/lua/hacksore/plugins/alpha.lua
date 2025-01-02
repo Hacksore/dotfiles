eae1572d3cea0ce13d8c83deef14cc50b9d634d3
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-01-01 21:28:20 -0600

    Add nice lil vite bootstrap util

--- .config/nvim/lua/hacksore/plugins/alpha.lua
+++ .config/nvim/lua/hacksore/plugins/alpha.lua
@@ -1,3 +1,47 @@
+local function create_new_ts_file()
+  vim.cmd("enew")
+  vim.cmd("file sandbox.ts")
+
+  -- Set filetype
+  vim.bo.filetype = "typescript"
+end
+
+vim.api.nvim_create_user_command("NewTSFile", create_new_ts_file, {})
+
+local function create_new_ts_project()
+  local ts_project_name = os.date("%Y-%m-%d-%H-%M-%S")
+  local ts_project_path = "/tmp/ts-sandbox/" .. ts_project_name
+  vim.cmd("silent !mkdir -p " .. ts_project_path)
+
+  vim.notify("Creating new TS project: " .. ts_project_path)
+  vim.cmd("silent cd " .. ts_project_path)
+  vim.cmd("silent !npx -y create-vite-app@latest . --template react-ts")
+
+  -- open the app tsx in a new buffer
+  vim.cmd("e src/App.tsx")
+
+  -- install the deps with pnpm
+  vim.notify("TS Sandbox installing deps with pnpm", "info", {})
+
+  vim.fn.jobstart("pnpm i", {
+    on_exit = function(_, exit_code)
+      if exit_code ~= 0 then
+        vim.notify("TS Sandbox failed to install deps", "error")
+        return
+      end
+
+      vim.notify("TS Sandbox setup completed!", "info")
+
+      -- give it some gitties
+      vim.cmd("silent !git init")
+      vim.cmd("silent !git add .")
+      vim.cmd("silent !git commit -m 'Initial commit'")
+    end,
+  })
+end
+
+vim.api.nvim_create_user_command("NewTSProject", create_new_ts_project, {})
+
 return {
   "goolord/alpha-nvim",
   event = "VimEnter",
@@ -20,6 +64,8 @@ return {
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button("wr", "󰁯  > Restore Session", "<cmd>SessionRestore<CR>"),
+      dashboard.button("nw", "  > New TS File", "<cmd>NewTSFile<CR>"),
+      dashboard.button("nw", "  > New TS Project", "<cmd>NewTSProject<CR>"),
       dashboard.button("ee", "  > Toggle file explorer", "<cmd>Neotree toggle<CR>"),
       dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
     }

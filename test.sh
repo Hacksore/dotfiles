266a0926e84a0b8bc3a2618762a2fe7dfe66b879
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-01-02 11:32:49 -0600

    This might work

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# used to test in a docker conainer
+
+apt update -y
+apt install curl git make cmake gcc fd-find ripgrep nodejs fzf -y
+mkdir -p ~/.config
+git clone https://github.com/Hacksore/dotfiles.git
+
+cd dofiles
+
+ln -s /dotfiles/.config/nvim ~/.config/nvim
+curl -sLO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+tar xzvf nvim-linux64.tar.gz
+CI_TEST=1 ./nvim-linux64/bin/nvim

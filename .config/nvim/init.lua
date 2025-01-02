e44a3895cab64d77bb233f2a4b103b64b4d7d018
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-01-02 10:27:17 -0600

    CI test

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,3 +15,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 vim.api.nvim_set_hl(0, "LazyGitBorder", { fg = "#0D1117", bg = "none" })
+
+-- if in CI_TEST close the nvim after lazy is done loading
+if os.getenv("CI_TEST") then
+  vim.defer_fn(function()
+    os.exit(0)
+  end, 0)
+end

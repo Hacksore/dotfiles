792965a54a2bdd1127fad09dc87931b1b7d66f1a
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-01-04 10:11:26 -0600

    Add dotfyle metadata

---
+++ .config/nvim/lua/hacksore/plugins/dotfyle.lua
@@ -0,0 +1,21 @@
+return {
+  "creativenull/dotfyle-metadata.nvim",
+  config = function()
+    local augroup = vim.api.nvim_create_augroup("DotfyleMetadataGroup", {})
+    vim.api.nvim_create_autocmd("User", {
+      group = augroup,
+      pattern = { "LazyUpdate", "LazyInstall", "LazySync", "LazyClean" },
+      once = true,
+      callback = function()
+        local dotfyle_metadata = require("dotfyle_metadata")
+        dotfyle_metadata.generate({})
+        local home = vim.fn.getenv("HOME")
+        vim.cmd("!npx prettier --write " .. dotfyle_metadata.dotfyle_path)
+        local move_dir_string = string.format("!mv %s %s", dotfyle_metadata.dotfyle_path,
+          home .. "/.config/nvim")
+        vim.cmd(move_dir_string)
+      end,
+    })
+  end,
+  event = "VeryLazy",
+}

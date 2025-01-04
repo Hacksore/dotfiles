78a0811ef3810ba76f8e28d443e544d8c5553744
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-01-04 11:17:22 -0600

    Fix search for todo-comment

--- .config/nvim/lua/hacksore/plugins/todo-comments.lua
+++ .config/nvim/lua/hacksore/plugins/todo-comments.lua
@@ -4,18 +4,19 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local todo_comments = require("todo-comments")
-
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-
-    keymap.set("n", "]t", function()
-      todo_comments.jump_next()
-    end, { desc = "Next todo comment" })
-
-    keymap.set("n", "[t", function()
-      todo_comments.jump_prev()
-    end, { desc = "Previous todo comment" })
-
-    todo_comments.setup()
+    todo_comments.setup({
+      search = {
+        command = "rg",
+        args = {
+          "--hidden",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+        },
+        pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+      },
+    })
   end,
 }

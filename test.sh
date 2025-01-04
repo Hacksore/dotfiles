51990abb38710ad95c20c4d19d61f29186796d7d
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-01-03 20:46:09 -0600

    Test quitting after config loads

--- test.sh
+++ test.sh
@@ -5,11 +5,13 @@
 apt update -y
 apt install curl git make cmake gcc fd-find ripgrep nodejs fzf -y
 mkdir -p ~/.config
-git clone https://github.com/Hacksore/dotfiles.git
+git clone https://github.com/Hacksore/dotfiles.git -b
 
 cd dofiles
 
 ln -s /dotfiles/.config/nvim ~/.config/nvim
 curl -sLO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
-CI=1 ./nvim-linux64/bin/nvim
+
+# run in background
+CI=1 ./nvim-linux64/bin/nvim --headless -eS -c "q!"

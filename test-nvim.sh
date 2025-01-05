348de04d2857dcff25ae46704b71282da4e17e7e
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-01-04 21:45:09 -0600

    Squanch exit code

--- test-nvim.sh
+++ test-nvim.sh
@@ -9,11 +9,11 @@ ln -s /app/dotfiles/.config/nvim ~/.config/nvim
 curl -sLO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
 tar xzf nvim-linux64.tar.gz
 
-mv /app/dotfiles/.config/nvim/lazy-lock.json /app/dotfiles/.config/nvim/lazy-lock.original.json
+mv ~/.config/nvim/lazy-lock.json ~/.config/nvim/lazy-lock.original.json
 
 # run in headless mode
 CI=1 ./nvim-linux64/bin/nvim --headless -c 'exe !!v:errmsg."cquit"'
 
-# diff the original and the generated lazy-lock.json
+# # diff the original and the generated lazy-lock.json
 echo "lazy diff lazy-lock.original.json -> lazy-lock.json"
-diff /app/dotfiles/.config/nvim/lazy-lock.original.json /app/dotfiles/.config/nvim/lazy-lock.json 
+diff -u ~/.config/nvim/lazy-lock.original.json ~/.config/nvim/lazy-lock.json && echo $? || true

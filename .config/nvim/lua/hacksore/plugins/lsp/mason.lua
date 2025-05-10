6f04047dc8a807bd02635e4173f47d00ad0c97de
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-05-10 11:30:27 -0500

    Migrate to mason v2

--- .config/nvim/lua/hacksore/plugins/lsp/mason.lua
+++ .config/nvim/lua/hacksore/plugins/lsp/mason.lua
@@ -1,62 +1,36 @@
+local LSP_LIST = {
+  "ts_ls",
+  "html",
+  "cssls",
+  "tailwindcss",
+  "ruby_lsp",
+  "lua_ls",
+  "rust_analyzer",
+  "dockerls",
+  "jsonls",
+  "jdtls",
+  "docker_compose_language_service",
+  "biome",
+  "astro",
+  "pylsp",
+  "yamlls",
+  "bashls",
+  "graphql",
+  "mdx_analyzer",
+  "prismals",
+}
+
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason-lspconfig.nvim",
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "mason-org/mason.nvim",
+    "neovim/nvim-lspconfig",
   },
   config = function()
-    -- import mason
-    local mason = require("mason")
-
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
-
-    local mason_tool_installer = require("mason-tool-installer")
-
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
+    require('mason').setup()
+    require('mason-lspconfig').setup({
+      ensure_installed = LSP_LIST,
+      automatic_enable = LSP_LIST
     })
-
-    mason_lspconfig.setup({
-      automatic_installation = true,
-      -- list of servers for mason to install
-      ensure_installed = {
-        "ts_ls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "ruby_lsp",
-        "lua_ls",
-        "rust_analyzer",
-        "dockerls",
-        "jsonls",
-        "jdtls",
-        "docker_compose_language_service",
-        "biome",
-        "astro",
-        "pylsp",
-        "yamlls",
-        "bashls",
-        "graphql",
-        "mdx_analyzer",
-        "prismals",
-      },
-    })
-
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "shellcheck",
-        "prettier", -- prettier formatter
-        "stylua",   -- lua formatter
-        "eslint",
-      },
-    })
-  end,
+  end
 }

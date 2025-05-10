44f69c6c793691f214dcb0673e358c57004b0540
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-05-10 13:42:00 -0500

    Move to blink cmp

---
+++ .config/nvim/lua/hacksore/plugins/blink-cmp.lua
@@ -0,0 +1,35 @@
+return {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = { 'rafamadriz/friendly-snippets' },
+  -- use a release tag to download pre-built binaries
+  build = 'cargo +nightly build --release',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'enter' },
+
+    appearance = {
+      nerd_font_variant = 'mono'
+    },
+    completion = { documentation = { auto_show = false } },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+    fuzzy = { implementation = "prefer_rust" }
+  },
+  opts_extend = { "sources.default" }
+}

06d5cb9fa6c1ce5bc23c243438cacce4089bba7a
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-05-11 14:28:12 -0500

    Fix schema stores maybe

--- .config/nvim/lua/hacksore/plugins/schema-store.lua
+++ .config/nvim/lua/hacksore/plugins/schema-store.lua
@@ -7,6 +7,14 @@ return {
         settings = {
           json = {
             schemas = require("schemastore").json.schemas(),
+            enable = false,
+          },
+          yaml = {
+            schemaStore = {
+              enable = false,
+              url = "",
+            },
+            schemas = require('schemastore').yaml.schemas(),
           },
         },
       })

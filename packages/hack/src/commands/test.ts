c395234ecfa8781941c76f2d7c900fd9de88b07f
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-10-17 07:03:54 -0500

    WIP

--- packages/hack/src/commands/test.ts
+++ packages/hack/src/commands/test.ts
@@ -4,38 +4,52 @@ import fs from "node:fs";
 import os from "node:os";
 import { argv } from "node:process";
 
+/**
+ * Spawns a Docker container to run tests if not already inside one.
+ * @returns {Promise<boolean>} - Returns true if a container was spawned, false otherwise.
+ */
+const spawnContainer = async () => {
+  const isInsideDocker = fs.existsSync('/.dockerenv');
+  console.log({ isInsideDocker });
+  if (isInsideDocker) {
+    return false
+  }
+
+  // spawn docker container
+  try {
+    await runCommand(
+      `docker run --rm ${IMAGE_NAME} ${argv.slice(3).join(" ")}`,
+    );
+  } catch (error) {
+    console.error("Test failed:", error.message);
+    process.exit(1);
+  }
+
+  return true;
+}
+
 export async function handleTest(options: {
   nightly: boolean;
   frozenLock: boolean;
   remote: boolean;
   skipCargo: boolean;
 }) {
-  const isInsideDocker = fs.existsSync('/.dockerenv');
+  const spawnedContainer = await spawnContainer();
+  if (spawnedContainer) {
+    return;
+  }
+
   const { frozenLock, remote, skipCargo } = options;
   const frozenLockfile = parseFlagToBoolean(frozenLock) ? "1" : "0";
   const useCargo = parseFlagToBoolean(skipCargo) ? "1" : "0";
   const useLocal = remote ? "0" : "1";
 
-  console.log({ options, argv, isInsideDocker, hostname: os.hostname() })
-
-  // spawn docker container
-  if (!isInsideDocker) {
-    try {
-      await runCommand(
-        `docker run --rm ${IMAGE_NAME} ${argv.slice(3).join(" ")}`,
-      );
-    } catch (error) {
-      console.error("Test failed:", error.message);
-      process.exit(1);
-    }
-  }
-
-  // logic that runs inside the docker container to test neovim
-  console.log("running in the docker container")
-
+  console.log({ options, argv, hostname: os.hostname() })
 
   try {
     await runCommand("/usr/local/bin/nvim-stable --version")
+    console.log("✅ nvim-stable version check passed")
   } catch (error) {
+    console.error("❌ nvim-stable version check failed:", error.message)
   }
 }

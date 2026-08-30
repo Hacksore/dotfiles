ec3d9774db984c521ae252021404709a4407e194
Sean Boult <996134+Hacksore@users.noreply.github.com>
2026-08-30 08:28:49 -0500

    Use latest toolchains

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
+export NONINTERACTIVE=1
+export CI=1
 export HOMEBREW_PATH="/home/linuxbrew/.linuxbrew"
 # NOTE: this exists but i don't know how to use it yet
 # https://docs.github.com/en/codespaces/setting-your-user-preferences/personalizing-github-codespaces-for-your-account#dotfiles
 
 # install brew
-NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
 
 # load brew
 eval "$($HOMEBREW_PATH/bin/brew shellenv)"
@@ -36,10 +38,6 @@ fi
 
 brew bundle --file="$DOTFILE_HOME/Brewfile.linux"
 
-# source nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$HOMEBREW_PATH/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PATH/opt/nvm/nvm.sh" # This loads nvm
-
 # Remove the ssh signing as github has some stuff to handle it via gpg
 git config --global credential.helper=/.codespaces/bin/gitcredential_github.sh
 git config --global --unset user.signingkey
@@ -47,7 +45,10 @@ git config --global --unset commit.gpgsign
 git config --global --unset gpg.format
 
 # install node
-nvm install 24 --lts
+eval "$(fnm env --shell zsh)"
+fnm install 24
+fnm default 24
+fnm use 24
 
 # source the rust env
 . "$HOME/.cargo/env"
@@ -56,11 +57,10 @@ nvm install 24 --lts
 rustup install stable
 rustup install nightly
 
-# move to the right dotfile repo and then pnpm i
+# Leave the container in the dotfiles repository. Interactive shells source
+# ~/.zshrc themselves; sourcing it here triggers fnm's interactive hook.
 if [[ "$GITHUB_REPOSITORY" = "Hacksore/dotfiles" ]]; then
 	cd /workspaces/dotfiles
 else
 	cd ~/dotfiles
 fi
-
-source "$HOME/.zshrc"
